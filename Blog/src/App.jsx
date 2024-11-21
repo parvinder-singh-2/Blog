45afd8b0b6da4e2f8503f5c8a8383a7b2986b853
parvinder-singh-2 <mr.parvindersinghgandhi@gmail.com>
2024-11-21 05:51:03 +0530

    completed basic file structure and functionalities

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import {login, logout} from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -20,7 +22,15 @@ function App() {
     .finally(() => setLoading(false))
   }, [])
 
-  return !loading ? false : null
+  return !loading ? <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header />
+      <main>
+        {/*<Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  </div> : null
 
 }
 

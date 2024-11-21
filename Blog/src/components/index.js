16a3bac8a8285be5e78ae781da39ca76db5bc79c
parvinder-singh-2 <mr.parvindersinghgandhi@gmail.com>
2024-11-21 07:42:39 +0530

    Made some imp components for reusability

---
+++ Blog/src/components/index.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBth"
+import Header from "./Header/Header"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+}
